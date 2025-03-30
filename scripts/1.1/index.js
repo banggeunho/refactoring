d7c95d338e41cc9cba2abf0fe1e14f786dde4ff4
banggeunho <panggeunho@gmail.com>
2025-03-30 22:30:16 +0900

    Refactor: 계산 데이터 추가 (customer, performances)

--- scripts/1.1/index.js
+++ scripts/1.1/index.js
@@ -6,18 +6,22 @@ console.log(result);
 
 function statement(invoice) {
     const statementData = {};
-    return renderPlainText(statementData, invoice);
+
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances;
+
+    return renderPlainText(statementData);
 }
 
-function renderPlainText(data, invoice) {
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+function renderPlainText(data) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
         result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     }
 
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+    result += `총액: ${usd(totalAmount(data))}\n`;
+    result += `적립 포인트: ${totalVolumeCredits(data)}점\n`;
 
     return result;
 }
@@ -27,17 +31,17 @@ function usd(aNumber) {
         {style: 'currency', currency: 'USD', minimumFractionDigits: 2}).format(aNumber / 100);
 }
 
-function totalAmount() {
+function totalAmount(data) {
     let result = 0;
-    for (let perf of invoices.performances) {
+    for (let perf of data.performances) {
         result += amountFor(perf);
     }
     return result;
 }
 
-function totalVolumeCredits() {
+function totalVolumeCredits(data) {
     let result = 0;
-    for (let perf of invoices.performances) {
+    for (let perf of data.performances) {
         result += volumeCreditsFor(perf);
     }
     return result;
