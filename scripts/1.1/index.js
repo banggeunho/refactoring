cb30e84a6cddd4bb3007eef6d84cf1b20b6f66a6
banggeunho <panggeunho@gmail.com>
2025-03-30 22:22:51 +0900

    Refactor: totalAmount 로직 함수로 추출

--- scripts/1.1/index.js
+++ scripts/1.1/index.js
@@ -4,17 +4,15 @@ const invoices = require('./invoices.json');
 const result = statement(invoices, plays);
 console.log(result);
 
-
 function statement(invoice) {
-    let totalAmount = 0;
-
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for (let perf of invoice.performances) {
         // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-        totalAmount += amountFor(perf);
     }
+
+    let totalAmount = applesauce(invoice);
 
     result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
@@ -25,6 +23,14 @@ function statement(invoice) {
 function usd(aNumber) {
     return new Intl.NumberFormat('en-US',
         {style: 'currency', currency: 'USD', minimumFractionDigits: 2}).format(aNumber / 100);
+}
+
+function applesauce(invoice) {
+    let totalAmount = 0;
+    for (let perf of invoice.performances) {
+        totalAmount += amountFor(perf);
+    }
+    return totalAmount;
 }
 
 function totalVolumeCredits() {
