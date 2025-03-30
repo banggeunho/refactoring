f723b872da20cc053bd7594d89f1187eedc5d224
banggeunho <panggeunho@gmail.com>
2025-03-30 22:56:42 +0900

    Refactor: 데이터 만드는 함수 별도 파일로 분리 createStatementData.js

--- scripts/1.1/index.js
+++ scripts/1.1/index.js
@@ -1,6 +1,7 @@
 import * as fs from 'node:fs';
 import path from 'path';
 import {fileURLToPath} from 'url';
+import createStatementData from './createStatementData.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,78 +15,8 @@ const invoices = JSON.parse(fs.readFileSync(invoicesPath, 'utf-8'));
 const result = statement(invoices, plays);
 console.log(result);
 
-function statement(invoice) {
-    return renderPlainText(createStatement(invoice));
-}
-
-function createStatement(invoice) {
-    const statementData = {};
-
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-
-    return statementData;
-}
-
-function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance); // shallow copy
-
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-
-    return result;
-}
-
-function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-}
-
-function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-        case 'tragedy':
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-
-        case 'comedy':
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-
-        default:
-            throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-
-    return result;
-}
-
-function volumeCreditsFor(aPerformance) {
-    let result = 0;
-
-    result += Math.max(aPerformance.audience - 30, 0);
-
-    if ('comedy' === aPerformance.play.type) {
-        result += Math.floor(aPerformance.audience / 5);
-    }
-    return result;
-}
-
-function totalAmount(data) {
-    return data.performances.reduce((total, p) => total + p.amount, 0);
-}
-
-function totalVolumeCredits(data) {
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+function statement(invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays));
 }
 
 function renderPlainText(data) {
