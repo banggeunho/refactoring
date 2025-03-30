84c7544d127e92823db0fa458d6d0e1379297fdc
banggeunho <panggeunho@gmail.com>
2025-03-30 22:15:04 +0900

    Refactor: volumeCredit 관련 로직 함수 추출

--- scripts/1.1/index.js
+++ scripts/1.1/index.js
@@ -16,10 +16,7 @@ function statement(invoice) {
         totalAmount += amountFor(perf);
     }
 
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-    }
+    let volumeCredits = totalVolumeCredits();
 
     result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
@@ -30,6 +27,14 @@ function statement(invoice) {
 function usd(aNumber) {
     return new Intl.NumberFormat('en-US',
         {style: 'currency', currency: 'USD', minimumFractionDigits: 2}).format(aNumber / 100);
+}
+
+function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoices.performances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
 }
 
 function volumeCreditsFor(aPerformance) {
