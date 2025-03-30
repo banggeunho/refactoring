edffac31baee324ee423843e86af7778fbc9f8ab
banggeunho <panggeunho@gmail.com>
2025-03-30 21:58:17 +0900

    Refactor: 가격 계산 함수 추출

--- scripts/1.1/index.js
+++ scripts/1.1/index.js
@@ -15,27 +15,7 @@ function statement(invoice, plays) {
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
 
-        let thisAmount = 0;
-
-        switch (play.type) {
-            case 'tragedy':
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-
-            case 'comedy':
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-
-            default:
-                throw new Error(`알 수 없는 장르: ${play.type}`);
-        }
+        let thisAmount = amountFor(play, perf);
 
         // 포인트 적립
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -54,5 +34,30 @@ function statement(invoice, plays) {
     result += `적립 포인트: ${volumeCredits}점\n`;
 
     return result;
+}
 
+function amountFor(play, perf) {
+    let thisAmount = 0;
+
+    switch (play.type) {
+        case 'tragedy':
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+
+        case 'comedy':
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+
+        default:
+            throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+    
+    return thisAmount;
 }
