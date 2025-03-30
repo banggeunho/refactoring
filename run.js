aa8fc408835488335adcfc1629b8f72daf6d5b6c
banggeunho <panggeunho@gmail.com>
2025-03-30 21:35:52 +0900

    Chore: Apply Eslint

--- run.js
+++ run.js
@@ -1,20 +1,20 @@
 // run.js
-const { spawn } = require("child_process");
-const path = require("path");
+const {spawn} = require('child_process');
+const path = require('path');
 
 const scriptName = process.argv[2];
 
 if (!scriptName) {
-    console.error("❌ 스크립트 이름을 입력하세요. 예: node run hello");
+    console.error('❌ 스크립트 이름을 입력하세요. 예: node run hello');
     process.exit(1);
 }
 
-const scriptPath = path.join(__dirname, "scripts", `${scriptName}.js`);
+const scriptPath = path.join(__dirname, 'scripts', `${scriptName}.js`);
 
-const child = spawn("node", [scriptPath, ...process.argv.slice(3)], {
-    stdio: "inherit",
+const child = spawn('node', [scriptPath, ...process.argv.slice(3)], {
+    stdio: 'inherit',
 });
 
-child.on("exit", (code) => {
+child.on('exit', (code) => {
     process.exit(code);
 });
