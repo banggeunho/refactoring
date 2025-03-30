618d0dd7b2e41ade4522654d97fec07b27bb65c0
banggeunho <panggeunho@gmail.com>
2025-03-30 23:04:12 +0900

    Feat: Statement Html 버전 추가

--- scripts/1.1/index.js
+++ scripts/1.1/index.js
@@ -12,8 +12,12 @@ const invoicesPath = path.join(__dirname, 'invoices.json');
 const plays = JSON.parse(fs.readFileSync(playsPath, 'utf-8'));
 const invoices = JSON.parse(fs.readFileSync(invoicesPath, 'utf-8'));
 
-const result = statement(invoices, plays);
-console.log(result);
+const plainText = statement(invoices, plays);
+console.log(plainText);
+
+const html = htmlStatement(invoices, plays);
+console.log(html);
+
 
 function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
@@ -28,6 +32,28 @@ function renderPlainText(data) {
 
     result += `총액: ${usd(data.totalAmount)}\n`;
     result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+
+    return result;
+}
+
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+
+    result += '<table>\n';
+    result += '<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>';
+
+    for (let perf of data.performances) {
+        result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}석</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+
+    result += '</table>\n';
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${usd(data.totalVolumeCredits)}</em>점</p>\n`;
 
     return result;
 }
